9f029d39385e2a4daae324bbd409390db60a6499
cherry.agrawal <cherryagrawal17@gmail.com>
2018-05-05 21:21:26 +0530

    Simple modification

--- app/src/androidTest/java/learning/projects/com/mylearningapp/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/learning/projects/com/mylearningapp/ExampleInstrumentedTest.java
@@ -22,5 +22,7 @@ public class ExampleInstrumentedTest {
         Context appContext = InstrumentationRegistry.getTargetContext();
 
         assertEquals("learning.projects.com.mylearningapp", appContext.getPackageName());
+
+        //Simple modification.
     }
 }
